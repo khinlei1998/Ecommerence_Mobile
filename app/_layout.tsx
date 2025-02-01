95c26f7e76cf1164ed399c40f4593925559fda9e
Yati <khinleiyatihtun@192.168.1.8>
2025-02-01 14:45:51 +0630

    Testing authication with context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,11 @@
 import { Slot } from 'expo-router';
 import { SessionProvider } from '@/provider/ctx';
-import { AuthContext } from '@/provider/auth_ctx';
+import AuthProvider from '@/provider/auth_ctx';
 export default function Root() {
   // Set up the auth context and render our layout inside of it.
   return (
-    <SessionProvider>
+    <AuthProvider>
       <Slot />
-    </SessionProvider>
+    </AuthProvider>
   );
 }
